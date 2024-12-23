54d1dd3e445a84ac0e65c753d2ae3cb1d3550554
HavenCross <havenquan@qq.com>
2024-12-23 17:11:20 +0800

    chore: update ecosystem.config.js to include multiple character files for the eliza script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'eliza',
       script: 'pnpm',
-      args: 'start --character="characters/trump.character.json"',
+      args: 'start --character="characters/c3po.character.json,characters/dobby.character.json,characters/eternalai.character.json,characters/tate.character.json,characters/trump.character.json"',
       autorestart: true,
       max_restarts: 5,
       min_uptime: '10s',
