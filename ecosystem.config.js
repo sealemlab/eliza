190da5d5a7b4d737825a06f55ce83edea26368fe
Yangx <641160771@qq.com>
2025-05-06 18:09:35 +0800

    Test running Xingye role

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,28 +1,16 @@
 module.exports = {
-  apps: [
-    {
-      name: 'eliza',
-      script: 'pnpm',
-      args: 'start --character="characters/cryptomaddoc.character.json,characters/cryptochili.character.json"',
-      autorestart: true,
-      max_restarts: 5,
-      min_uptime: '10s',
-      restart_delay: 5000,
-      out_file: 'logs/eliza/normal.log',
-      error_file: 'logs/eliza/error.log',
-      combine_logs: true,
-    },
-    {
-      name: 'eliza-client',
-      script: 'pnpm',
-      args: 'start:client',
-      autorestart: true,
-      max_restarts: 5,
-      min_uptime: '10s',
-      restart_delay: 5000,
-      out_file: 'logs/eliza-client/normal.log',
-      error_file: 'logs/eliza-client/error.log',
-      combine_logs: true,
-    },
-  ]
+    apps: [
+        {
+            name: "eliza-xingye",
+            script: "pnpm",
+            args: 'start --characters="characters/xingye.character.json"',
+            autorestart: true,
+            max_restarts: 5,
+            min_uptime: "10s",
+            restart_delay: 5000,
+            out_file: "logs/eliza-xingye/normal.log",
+            error_file: "logs/eliza-xingye/error.log",
+            combine_logs: true,
+        },
+    ],
 };
